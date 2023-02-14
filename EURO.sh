8f49d546d68d6cce603ae0fad8d93f0c51318fb1
root <root@pi-star>
2023-02-14 14:42:32 +0100

    created w0chp.txt as option for other system

---
+++ EURO.sh
@@ -0,0 +1,6 @@
+#bin
+#bash
+
+sudo git pull
+
+sudo cp /var/euro/*.sh /usr/local/sbin
--- upeuro.sh
+++ upeuro.sh
@@ -2,8 +2,14 @@
 
 sudo mount -o remount,rw /
 
+FILE=/var/euro/w0chp.txt
+if [ -f "$FILE" ]; then
+    sudo patch /var/www/dashboard/admin/advanced/header-menu.inc /var/euro/eurow0chpheader.patch
+else 
+    sudo patch /var/www/dashboard/admin/expert/header-menu.inc /var/euro/euroheader.patch
 
-        sudo patch /var/www/dashboard/admin/expert/header-menu.inc /var/euro/euroheader.patch
+fi
+
 
 
 

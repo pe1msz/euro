c4198cf34933a6c1a1d62b11e333b19e77863dd7
root <root@pi-star>
2023-02-14 14:47:27 +0100

    added w0chp-patch

--- upeuro.sh
+++ upeuro.sh
@@ -4,7 +4,7 @@ sudo mount -o remount,rw /
 
 FILE=/var/euro/w0chp.txt
 if [ -f "$FILE" ]; then
-    sudo patch /var/www/dashboard/admin/advanced/header-menu.inc /var/euro/eurow0chpheader.patch
+    sudo patch /var/www/dashboard/admin/advanced/header-menu.inc /var/euro/eurow0chphead.patch
 else 
     sudo patch /var/www/dashboard/admin/expert/header-menu.inc /var/euro/euroheader.patch
 
